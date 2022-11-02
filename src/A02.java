915d0c28a30281dec9bae5213c30c9eb5fd2a78a
{Nesrin <{ng.erkan@gmail.com}>
2022-11-02 18:01:51 +0100

    1.commit

---
+++ src/A02.java
@@ -0,0 +1,15 @@
+public class A02 {
+    public static void main(String[] args) {
+
+// asagida (consol'da) terminal' e tikladik ve git init yazarak ilk git koutunu verdik
+        // bunu yapinca, bu intellij projesinin kayitli oldugu klasorde bir git dosyasi (gizli) olusturdu
+        // yandaki proje ekraninda yeni proje ve altidaki A1 A2 class'lari kirmizi renk aldi, bunun anlami,
+        // bu dosyalari git ile iliskilendirdin ancak repository' e bir version olarak kaydedilmedi
+
+
+        // 2. komut bu repository kaydi icin
+        // git add. herseyi ekle
+        // tek bir dosya ekleyeceksek git add.A01
+
+    }
+}
