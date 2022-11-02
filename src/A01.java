4dcf7282d256abfbf0cef090c53aed14b968413b
{Nesrin <{ng.erkan@gmail.com}>
2022-11-02 18:15:20 +0100

    first commit

--- src/A01.java
+++ src/A01.java
@@ -2,6 +2,9 @@ public class A01 {
     public static void main(String[] args) {
 
         System.out.println("Hello github");
+        System.out.println("Hello git");
+        System.out.println("Hello hub");
+
 
     }
 }
--- src/A02.java
+++ src/A02.java
@@ -1,15 +1,22 @@
 public class A02 {
-    public static void main(String[] args) {
 
-// asagida (consol'da) terminal' e tikladik ve git init yazarak ilk git koutunu verdik
+        // asagida (consol'da) terminal' e tikladik ve git init yazarak ilk git koutunu verdik
         // bunu yapinca, bu intellij projesinin kayitli oldugu klasorde bir git dosyasi (gizli) olusturdu
         // yandaki proje ekraninda yeni proje ve altidaki A1 A2 class'lari kirmizi renk aldi, bunun anlami,
         // bu dosyalari git ile iliskilendirdin ancak repository' e bir version olarak kaydedilmedi
 
 
         // 2. komut bu repository kaydi icin
-        // git add. herseyi ekle
+        // git add . herseyi ekle
         // tek bir dosya ekleyeceksek git add.A01
+        //yaptigimiz son versiondan sonraki tum degisiklileri gondermek icin bu komut
+
+
+        // 3. respository icin son komut
+        // git commit -m "   " tirnak icine biz seciyoruz ne yazacagimizi
+
+    public static void main(String[] args) {
 
     }
+
 }
